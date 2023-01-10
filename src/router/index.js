397f7f9a8dd629bfe39a0da840b4d2e89633e5b5
Fernweh99 <daniele.dimento@gmail.com>
2023-01-10 12:02:56 +0100

    init proj

--- src/router/index.js
+++ src/router/index.js
@@ -4,17 +4,15 @@ import HomeView from "../views/HomeView.vue";
 const routes = [
   {
     path: "/",
-    name: "home",
+    name: "Home",
     component: HomeView,
+    props: { name: "type" },
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/:name",
+    name: "avatar",
+    component: HomeView,
+    props: true,
   },
 ];
 
